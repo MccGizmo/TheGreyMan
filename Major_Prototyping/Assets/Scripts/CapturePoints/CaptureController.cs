3080ef7598a3030cf05d14ac5a90b3a213c4e966
Jai Gaddes <jai.gaddes@gmail.com>
2016-08-07 12:45:46 +1000

    CapturePoints Prototype Readded

---
+++ Major_Prototyping/Assets/Scripts/CapturePoints/CaptureController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptureController : MonoBehaviour {
+
+    public bool blueTeam = false;
+    public bool redTeam = false;
+    public GameObject[] zoneOnes;
+    public Material[] redMat;
+    public Material[] blueMat;
+    public Material[] neutralMat;
+
+    public float blueCapturePerc = 0;
+    public float redCapturePerc = 0;
+
+    void Update() {
+
+        if (blueTeam == true) {
+
+            blueCapturePerc += Time.deltaTime * 10;
+            redCapturePerc -= Time.deltaTime * 10;
+        }
+
+        if (redTeam == true)
+        {
+
+            blueCapturePerc -= Time.deltaTime * 10;
+            redCapturePerc += Time.deltaTime * 10;
+        }
+
+        if (redTeam == true && blueTeam == true) {
+
+            redCapturePerc = redCapturePerc;
+            blueCapturePerc = blueCapturePerc;
+        }
+
+        if (redCapturePerc >= 100) {
+
+            redCapturePerc = 100;
+
+            zoneOnes = GameObject.FindGameObjectsWithTag ("ZoneOne");
+            foreach (GameObject go in zoneOnes) {
+
+                MeshRenderer[] meshRenderers = go.GetComponentsInChildren<MeshRenderer>();
+                foreach (MeshRenderer r in meshRenderers)
+                {
+
+                    foreach (Material m in r.materials) {
+
+                        r.materials = redMat;
+                    }
+                }
+            }
+        }
+
+        if (blueCapturePerc >= 100)
+        {
+            
+            blueCapturePerc = 100;
+
+            zoneOnes = GameObject.FindGameObjectsWithTag("ZoneOne");
+            foreach (GameObject go in zoneOnes)
+            {
+
+                MeshRenderer[] meshRenderers = go.GetComponentsInChildren<MeshRenderer>();
+                foreach (MeshRenderer r in meshRenderers){
+                    
+                    foreach (Material m in r.materials){
+
+                        r.materials = blueMat;
+                    }
+                }
+            }
+        }
+
+        if (blueCapturePerc <= 50 && redCapturePerc <= 51)
+        {
+
+            zoneOnes = GameObject.FindGameObjectsWithTag("ZoneOne");
+            foreach (GameObject go in zoneOnes){
+
+                MeshRenderer[] meshRenderers = go.GetComponentsInChildren<MeshRenderer>();
+                foreach (MeshRenderer r in meshRenderers){
+
+                    foreach (Material m in r.materials){
+
+                        r.materials = neutralMat;
+                    }
+                }
+            }
+        }
+
+        if (redCapturePerc <= 0) {
+
+            redCapturePerc = 0;
+        }
+
+        if (blueCapturePerc <= 0)
+        {
+
+            blueCapturePerc = 0;
+        }
+    }
+}
---
+++ Major_Prototyping/Assets/Scripts/CapturePoints/FlagCapture.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlagCapture : MonoBehaviour {
+
+    private CaptureController _CaptureController;
+
+    void Start() {
+
+        _CaptureController = GameObject.Find("TriggerArea").GetComponent<CaptureController>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.tag == "BlueTeam") {
+
+            _CaptureController.blueTeam = true;
+        }
+
+        if (other.tag == "RedTeam")
+        {
+
+            _CaptureController.redTeam = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "BlueTeam")
+        {
+
+            _CaptureController.blueTeam = false;
+        }
+
+        if (other.tag == "RedTeam")
+        {
+
+            _CaptureController.redTeam = false;
+        }
+    }
+}
