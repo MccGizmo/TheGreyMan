9f38e6894b8d8a16cde901c26bc0f9d8799d532f
LJRam <lachlanjms@gmail.com>
2016-08-16 19:20:23 +1000

    Just some Blending setting up - LJ

---
+++ Major_Prototyping/Assets/Scripts/Player/PlayerBlending.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBlending : MonoBehaviour {
+
+    public Color color = new Color(148.0F, 211.0F, 255.0F, 255.0F);
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (CompareTag("Civilian"))
+        {
+
+        }
+    }
+}
